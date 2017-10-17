7aeec2903273853dbf251ff9877a21e4eb99f923
christenc3 <31932938+christenc3@users.noreply.github.com>
2017-10-17 14:06:20 -0400

    Add files via upload
    File Explorer Works

--- PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/MainForm.h
+++ PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/MainForm.h
@@ -44,6 +44,7 @@ namespace SoftwareIntegrityTester {
 
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::ListBox^  listBox1;
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 
 
 	private:
@@ -67,6 +68,7 @@ namespace SoftwareIntegrityTester {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -79,36 +81,40 @@ namespace SoftwareIntegrityTester {
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(488, 28);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(4, 2, 0, 2);
+			this->menuStrip1->Size = System::Drawing::Size(368, 24);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
 			// filterToolStripMenuItem
 			// 
 			this->filterToolStripMenuItem->Name = L"filterToolStripMenuItem";
-			this->filterToolStripMenuItem->Size = System::Drawing::Size(54, 24);
+			this->filterToolStripMenuItem->Size = System::Drawing::Size(45, 20);
 			this->filterToolStripMenuItem->Text = L"Filter";
 			// 
 			// helpToolStripMenuItem
 			// 
 			this->helpToolStripMenuItem->Name = L"helpToolStripMenuItem";
-			this->helpToolStripMenuItem->Size = System::Drawing::Size(53, 24);
+			this->helpToolStripMenuItem->Size = System::Drawing::Size(44, 20);
 			this->helpToolStripMenuItem->Text = L"Help";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(12, 378);
+			this->button1->Location = System::Drawing::Point(9, 307);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(120, 60);
+			this->button1->Size = System::Drawing::Size(90, 49);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Open";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(138, 378);
+			this->button2->Location = System::Drawing::Point(104, 307);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(120, 60);
+			this->button2->Size = System::Drawing::Size(90, 49);
 			this->button2->TabIndex = 2;
 			this->button2->Text = L"Run";
 			this->button2->UseVisualStyleBackColor = true;
@@ -116,28 +122,33 @@ namespace SoftwareIntegrityTester {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(396, 424);
+			this->label1->Location = System::Drawing::Point(297, 344);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(92, 17);
+			this->label1->Size = System::Drawing::Size(69, 13);
 			this->label1->TabIndex = 4;
 			this->label1->Text = L"Version 1.0.0";
 			// 
 			// listBox1
 			// 
 			this->listBox1->FormattingEnabled = true;
-			this->listBox1->ItemHeight = 16;
 			this->listBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Test", L"Test2", L"Test3" });
-			this->listBox1->Location = System::Drawing::Point(12, 42);
+			this->listBox1->Location = System::Drawing::Point(9, 34);
+			this->listBox1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(464, 308);
+			this->listBox1->Size = System::Drawing::Size(349, 251);
 			this->listBox1->TabIndex = 5;
 			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainForm::listBox1_SelectedIndexChanged);
 			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
 			// MainForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(488, 450);
+			this->ClientSize = System::Drawing::Size(368, 373);
 			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button2);
@@ -145,10 +156,10 @@ namespace SoftwareIntegrityTester {
 			this->Controls->Add(this->menuStrip1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MainMenuStrip = this->menuStrip1;
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->MaximizeBox = false;
-			this->MaximumSize = System::Drawing::Size(506, 497);
-			this->MinimumSize = System::Drawing::Size(506, 497);
+			this->MaximumSize = System::Drawing::Size(384, 411);
+			this->MinimumSize = System::Drawing::Size(384, 411);
 			this->Name = L"MainForm";
 			this->Text = L"Software Integrity Tester";
 			this->menuStrip1->ResumeLayout(false);
@@ -161,6 +172,15 @@ namespace SoftwareIntegrityTester {
 	private: System::Void textBox3_TextChanged(System::Object^  sender, System::EventArgs^  e) {
 	}
 private: System::Void listBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+		System::IO::StreamReader ^ sr = gcnew
+			System::IO::StreamReader(openFileDialog1->FileName);
+		MessageBox::Show(sr->ReadToEnd());
+		sr->Close();
+	}
 }
 };
 }
