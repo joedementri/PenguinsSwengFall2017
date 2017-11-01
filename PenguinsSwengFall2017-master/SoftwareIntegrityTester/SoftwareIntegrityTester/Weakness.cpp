27cc07029ff5fe334b51e1d1e16916347bfe5c7f
TylerLig <ligenzowt5@students.rowan.edu>
2017-11-01 01:28:22 -0400

    Added filter menu and planned weaknesses

--- PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/Weakness.cpp
+++ PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/Weakness.cpp
@@ -7,17 +7,45 @@
 
 		Weakness::Weakness() {
 			name = "default name";
-			priority = Priority::Low;
+			priority = Priority::Normal;
 			risk = Risk::Low;
-			solution = "default solution";
+			solution = "";
+			cwe = System::Int32(-1);
 		}
-		Weakness::Weakness(String^ n, Priority p, Risk r, String^ s) {			
-			name = n;
-			priority = p;
-			risk = r;
-			solution = s;
+		Weakness::Weakness(String^ name, Risk risk) : Weakness() {
+			this->name = name;
+			this->risk = risk;
+		}
+		Weakness::Weakness(String^ name, Risk risk, int^ cwe) : Weakness(){
+			this->name = name;
+			this->risk = risk;
+			this->cwe = cwe;
+		}
+
+		//weakness report text for this weakness
+		String^ Weakness::getWreport() {
+			return wreport;
+		}
+		String^ Weakness::getName() {
+			return name;
+		}
+		String^ Weakness::getCweLink() {
+			if (cwe->CompareTo(-1).Equals(1)) {
+				return gcnew System::String("https://cwe.mitre.org/data/definitions/" + cwe + ".html");
+			}
+			return "";
 		}
 
 		String^ Weakness::toString() {
-				return name + "\nPriority: " + priority.ToString() + "\nRisk: " + risk.ToString() + "\nSolution:\n" + solution + "\n";
+			String^ s = "";
+			if (cwe->CompareTo(-1).Equals(1))
+				s += "CWE-" + cwe + ": ";			
+			s += name + "\nRisk: " + risk.ToString();
+			if (!solution->IsNullOrWhiteSpace(solution))
+				s += "\nSolution:\n" + solution + "\n";
+			
+			return s;
 			}
+		String^ Weakness::ToString() {
+			return name;
+		}
--- PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/Weakness.h
+++ PenguinsSwengFall2017-master/SoftwareIntegrityTester/SoftwareIntegrityTester/Weakness.h
@@ -5,15 +5,22 @@ ref class Weakness
 {
 	public:
 		String^ toString();
+		String^ ToString() override;
+		String^ getWreport();
+		String^ getName();
+		String^ getCweLink();
 		enum class Priority { Low, Normal, Critical };
 		enum class Risk { Low, Med, High };
 
 		Weakness();
-		Weakness(String^ n, Priority p, Risk r, String^ s);
+		Weakness(String^ name, Risk risk);
+		Weakness(String^ name, Risk risk, int^ cwe);
 	private:
 		String^ name; 
 		Priority priority;
-		Risk risk; 
+		Risk risk;
 		String^ solution;
+		String^ wreport;
+		int^ cwe;
 };
 
