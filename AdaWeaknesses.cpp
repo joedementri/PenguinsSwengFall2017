98b572a2a243c45898dd3b5dc68fecdd9dc2e30e
Joseph Dementri <joedementri42012@gmail.com>
2017-12-18 11:20:00 -0500

    All Weaknesses Except 1 and 4 Work

--- AdaWeaknesses.cpp
+++ AdaWeaknesses.cpp
@@ -268,7 +268,7 @@ void AdaWeaknesses::checkForWeakness6(std::ofstream& mystream, string fileConten
 	rightParPos = fileContents.find(rightPar, rightParPos+1);
       }
 
-      string constraint = fileContents.substr(lastLeftParPos, (rightParPos-lastLeftParPos));
+      string constraint = fileContents.substr(lastLeftParPos, (rightParPos-lastLeftParPos)+1);
       mystream << "\n\tArray on Line " << lineNum << " has range constraints: " << constraint;
 
       
@@ -281,17 +281,382 @@ void AdaWeaknesses::checkForWeakness6(std::ofstream& mystream, string fileConten
 }
 
 void AdaWeaknesses::checkForWeakness7(std::ofstream& mystream, string fileContents) {
-  mystream << "\nTestLine7";
+  mystream << "\nExpression Alwasys True:\n";
+
+  //Keywords
+  string ifKW = "if";
+  string thenKW = "then";
+
+  //Initial Position
+  int thenPos = fileContents.find(thenKW);
+
+  //If Not Found
+  if (thenPos == -1) { 
+    mystream << "\n\tNo If-Then Statements Found.";
+  }
+
+  //Find all if then statements
+  while (thenPos != -1) {
+
+    //Get Line Num
+    int lineNum = 0;
+    int newlinePos = fileContents.find("\n");
+    while (newlinePos < thenPos) {
+      lineNum++;
+      newlinePos = fileContents.find("\n", newlinePos+1);
+    }
+
+    //Find position of if KW
+    int ifPos = fileContents.find(ifKW);
+    int lastIfPos = ifPos;
+    while (ifPos != -1 && ifPos < thenPos) {
+      lastIfPos = ifPos;
+      ifPos = fileContents.find(ifKW, ifPos+1);
+    }
+
+    //Pull out everything between if and then
+    int beginningPos = lastIfPos+3;
+    string condExp = fileContents.substr(beginningPos, (thenPos-beginningPos));
+
+    mystream << "\n\tIf-Then Condition " << condExp << " found on line: " << lineNum;
+    mystream << "\n\t\tPlease make sure that " << condExp << " will not always be true.";
+
+    thenPos = fileContents.find(thenKW, thenPos+1);
+	   
+  }  
+  mystream << "\n";
 }
 
 void AdaWeaknesses::checkForWeakness8(std::ofstream& mystream, string fileContents) {
-  mystream << "\nTestLine8";
+  mystream << "\nExpression Always False:\n";
+
+  //Keywords
+  string ifKW = "if";
+  string thenKW = "then";
+
+  //Initial Position
+  int thenPos = fileContents.find(thenKW);
+
+  //Find all if then statements
+  while (thenPos != -1) {
+
+    //Get Line Num
+    int lineNum = 0;
+    int newlinePos = fileContents.find("\n");
+    while (newlinePos < thenPos) {
+      lineNum++;
+      newlinePos = fileContents.find("\n", newlinePos+1);
+    }
+
+    //Find position of if KW
+    int ifPos = fileContents.find(ifKW);
+    int lastIfPos = ifPos;
+    while (ifPos != -1 && ifPos < thenPos) {
+      lastIfPos = ifPos;
+      ifPos = fileContents.find(ifKW, ifPos+1);
+    }
+
+    //Pull out everything between if and then
+    int beginningPos = lastIfPos+3;
+    string condExp = fileContents.substr(beginningPos, (thenPos-beginningPos));
+
+    mystream << "\n\tIf-Then Condition " << condExp << " found on line: " << lineNum;
+    mystream << "\n\t\tPlease make sure that " << condExp << " will not always be false.";
+
+    thenPos = fileContents.find(thenKW, thenPos+1);
+	   
+  }
+  mystream << "\n";
 }
 
 void AdaWeaknesses::checkForWeakness9(std::ofstream& mystream, string fileContents) {
-  mystream << "\nTestLine9";
+  mystream << "\nRedundant Conditionals\n";
+
+  //Look for and or or
+  string andKW = "and";
+  string orKW = " or";
+  string xorKW = "xor";
+  string neq = "/=";
+  string eq = "=";
+  string lt = "<";
+  string lteq = "<=";
+  string gt = ">";
+  string gteq = ">=";
+  string ifKW = "if";
+  string thenKW = "then";
+
+  //Initial Position
+  int thenPos = fileContents.find(thenKW);
+
+  //Find all if then statements
+  while (thenPos != -1) {
+
+    //Get Line Num
+    int lineNum = 0;
+    int newlinePos = fileContents.find("\n");
+    while (newlinePos < thenPos) {
+      lineNum++;
+      newlinePos = fileContents.find("\n", newlinePos+1);
+    }
+
+    //Find position of if KW
+    int ifPos = fileContents.find(ifKW);
+    int lastIfPos = ifPos;
+    while (ifPos != -1 && ifPos < thenPos) {
+      lastIfPos = ifPos;
+      ifPos = fileContents.find(ifKW, ifPos+1);
+    }
+
+    //Pull out everything between if and then
+    int beginningPos = lastIfPos+3;
+    string condExp = fileContents.substr(beginningPos, (thenPos-beginningPos));
+
+    //pull out each var in string that isn't a delimiter
+
+    string leftBooleanOp;
+    string rightBooleanOp;
+
+    //--------
+    //AND
+    //--------
+    int andPos = condExp.find(andKW);
+    if (andPos != -1) {
+      //find space before left side
+      int spacePos = condExp.find(" ");
+      
+      if (spacePos == -1) {
+	
+	leftBooleanOp = condExp.substr(0, andPos);
+	
+      } else {
+	
+	leftBooleanOp = condExp.substr(spacePos, (andPos-spacePos));
+	
+      }
+
+      //find space after right side
+      int rightSpacePos = condExp.find(" ", andPos+4);
+
+      if (rightSpacePos == -1) {
+	
+	rightBooleanOp = condExp.substr(andPos+3);
+	
+      } else {
+
+	rightBooleanOp = condExp.substr(andPos+3, (rightSpacePos-(andPos+3)));
+	
+      }
+
+      mystream << "\n\tAND Expression Found at Line " << lineNum;
+      mystream << "\n\t\tLeft Side: " << leftBooleanOp;
+      mystream << "\n\t\tRight Side: " << rightBooleanOp;
+
+      
+    }
+
+    //-------
+    //OR
+    //-------
+    int orPos = condExp.find(orKW);
+    if (orPos != -1) {
+      //find space before left side
+      int spacePos = condExp.find(" ");
+      
+      if (spacePos == -1) {
+	
+	leftBooleanOp = condExp.substr(0, orPos);
+	
+      } else {
+	
+	leftBooleanOp = condExp.substr(spacePos, (orPos-spacePos));
+	
+      }
+
+      //find space after right side
+      int rightSpacePos = condExp.find(" ", orPos+4);
+
+      if (rightSpacePos == -1) {
+	
+	rightBooleanOp = condExp.substr(orPos+3);
+	
+      } else {
+
+	rightBooleanOp = condExp.substr(orPos+3, (rightSpacePos-(orPos+3)));
+	
+      }
+
+      mystream << "\n\tOR Expression Found at Line " << lineNum;
+      mystream << "\n\t\tLeft Side: " << leftBooleanOp;
+      mystream << "\n\t\tRight Side: " << rightBooleanOp;
+
+      
+    }
+
+    //------
+    //XOR
+    //------
+    
+    int xorPos = condExp.find(xorKW);
+    if (xorPos != -1) {
+      //find space before left side
+      int spacePos = condExp.find(" ");
+      
+      if (spacePos == -1) {
+	
+	leftBooleanOp = condExp.substr(0, xorPos);
+	
+      } else {
+	
+	leftBooleanOp = condExp.substr(spacePos, (xorPos-spacePos));
+	
+      }
+
+      //find space after right side
+      int rightSpacePos = condExp.find(" ", xorPos+4);
+
+      if (rightSpacePos == -1) {
+	
+	rightBooleanOp = condExp.substr(xorPos+3);
+	
+      } else {
+
+	rightBooleanOp = condExp.substr(xorPos+3, (rightSpacePos-(xorPos+3)));
+	
+      }
+
+      mystream << "\n\tXOR Expression Found on Line " << lineNum;
+      mystream << "\n\t\tLeft Side: " << leftBooleanOp;
+      mystream << "\n\t\tRight Side: " << rightBooleanOp;
+
+      
+    }
+
+
+    //TODO Later:
+    int neqPos = condExp.find(neq);
+    int eqPos = condExp.find(eq);
+    int ltPos = condExp.find(lt);
+    int lteqPos = condExp.find(lteq);
+    int gtPos = condExp.find(gt);
+    int gteqPos = condExp.find(gteq);
+
+    
+
+    thenPos = fileContents.find(thenKW, thenPos+1);
+	   
+  }
+  mystream << "\n";
 }
 
 void AdaWeaknesses::checkForWeakness10(std::ofstream& mystream, string fileContents) {
-  mystream << "\nTestLine10";
+  mystream << "\nInfinite Loops:\n";
+
+  //Keywords
+  string loopKW = "loop";
+  string endLoop = "end loop";
+
+  //Initial
+  int loopPos = fileContents.find(loopKW);
+
+  //if no loops
+  if (loopPos == -1) {
+    mystream << "\nNo loops found.";
+  }
+
+  //
+  while (loopPos != -1) {
+
+    //Get Line Num
+    int lineNum = 0;
+    int newlinePos = fileContents.find("\n");
+    while (newlinePos < loopPos) {
+      lineNum++;
+      newlinePos = fileContents.find("\n", newlinePos+1);
+    }
+
+    //Make sure current line isnt while, for, endloop
+    //find while
+    int whilePos = fileContents.find("while");
+    int lastWhilePos = whilePos;
+    while (whilePos < loopPos && whilePos != -1) {
+      lastWhilePos = whilePos;
+      whilePos = fileContents.find("while", whilePos+1);
+    }
+
+    //find while line num
+    int whilePosLine = 0;
+    int whileNewlinePos = fileContents.find("\n");
+    while (whileNewlinePos < lastWhilePos) {
+      whilePosLine++;
+      whileNewlinePos = fileContents.find("\n", whileNewlinePos+1);
+    }
+
+    //find for
+    int forPos = fileContents.find("for");
+    int lastForPos = forPos;
+    while (forPos < loopPos && forPos != -1) {
+      lastForPos = forPos;
+      forPos = fileContents.find("for", forPos+1);
+    }
+
+    //find for line num
+    int forPosLine = 0;
+    int forNewlinePos = fileContents.find("\n");
+    while (forNewlinePos < lastForPos) {
+      forPosLine++;
+      forNewlinePos = fileContents.find("\n", forNewlinePos+1);
+    }
+
+    //find end
+    int endPos = fileContents.find("end loop");
+    int lastEndPos = endPos;
+    while (endPos < loopPos && endPos != -1) {
+      lastEndPos = endPos;
+      endPos = fileContents.find("end loop", endPos+1);
+    }
+
+    //find end line num
+    int endPosLine = 0;
+    int endNewlinePos = fileContents.find("\n");
+    while (endNewlinePos < lastEndPos) {
+      endPosLine++;
+      endNewlinePos = fileContents.find("\n", endNewlinePos+1);
+    }
+
+    //Determines if loop is while,foor, or end loop instead of just loop
+    if (whilePosLine != lineNum && forPosLine != lineNum && endPosLine != lineNum) {
+
+      //find end loop
+      int endLoopPos = fileContents.find(endLoop);
+      while (endLoopPos < loopPos) {
+	endLoopPos = fileContents.find(endLoop, endLoopPos+1);
+      }
+
+      //Get Line Num of Loop
+      int endLineNum = 0;
+      int otherNewlinePos = fileContents.find("\n");
+      while (otherNewlinePos < endLoopPos) {
+	endLineNum++;
+	otherNewlinePos = fileContents.find("\n", otherNewlinePos+1);
+      }
+
+      string insideOfLoop = fileContents.substr(loopPos+4, (endLoopPos-(loopPos+4)));
+
+      mystream << "\n\tLoop from Lines " << lineNum << " - " << endLineNum; 
+
+      //see if loop has exit case
+      int exitPos = insideOfLoop.find("exit");
+
+      if (exitPos == -1) {
+	mystream << "\n\t\tLoop DOES NOT have Exit case";
+      } else {
+	mystream << "\n\t\tLoop HAS Exit case";
+      }
+
+    }
+
+    loopPos = fileContents.find(loopKW, loopPos+1);
+  }
+    
+  mystream << "\n";
 }
